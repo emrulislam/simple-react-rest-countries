28f3a25491c1552c3eef8e7d9d2495bf90138ce1
Emrul Islam <emrul.mpl@gmail.com>
2025-09-18 19:49:50 +0600

    implemented daisyui card styling

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,16 +1,20 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
 
-const Countries = ({countriesPromise}) => {
-    const countriesData= use(countriesPromise)
-    const countries=countriesData.countries
-    console.log(countries);
-    return (
-        <div>
-            <p>Total Countries:{countries.length}</p>
-            <Country countries={countries}></Country>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+  return (
+    <div>
+      <p>Total Countries:{countries.length}</p>
+      <div className="grid grid-cols-3 gap-4">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Country = ({countries}) => {
-    console.log(countries)
-    return (
-        <div>
-            {
-                countries.map((country=>
-                    <h3>Name:{country.name.common}</h3>
-                ))
-            }
+const Country = ({ country }) => {
+  console.log(country);
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{country.name.common}</h2>
+          <p>{country.flags.flags.alt}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Country;
